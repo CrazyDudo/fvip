346fc4e31c72f306b961b95a3fd8c3329b6edeae
crazydudo <crazydudo>
2019-05-17 10:59:21 +0800

    update nothing :)

--- app/src/main/java/com/fvip/cd/fvipplayer/AppApplication.java
+++ app/src/main/java/com/fvip/cd/fvipplayer/AppApplication.java
@@ -1,6 +1,11 @@
 package com.fvip.cd.fvipplayer;
 
 import android.app.Application;
+
+
+import cn.jiguang.analytics.android.api.JAnalyticsInterface;
+import cn.jpush.android.api.JPushInterface;
+
 /**
  * Created by cd on 2018/7/19.
  */
@@ -12,6 +17,13 @@ public class AppApplication extends Application {
     public void onCreate() {
         super.onCreate();
         mApplication = this;
+        initJpush();
+    }
+
+    private void initJpush() {
+        JPushInterface.init(this);
+        JPushInterface.setDebugMode(true);//如果时正式版就改成false
+        JAnalyticsInterface.init(this);
     }
 
     public static Application getContext() {
